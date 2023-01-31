d228e6f941e89056ad4c1c267a168a51837faf77
mahfuz khan <mahfuzpust@gmail.com>
2023-01-31 18:29:50 +0600

    Get

---
+++ MagicVilla_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,18 @@
+﻿using MagicVilla_API.Data;
+using MagicVilla_API.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        } 
+    }
+}
+
---
+++ MagicVilla_API/Models/DTO/VillaDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MagicVilla_API.Models.DTO
+{
+    public class VillaDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int sqrt { get; set; }
+        public int Occupancy { get; set; }
+
+    }
+}
