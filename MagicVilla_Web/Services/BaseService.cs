e7c2d4186443d338a3d5164155fa997cfdff1045
mahfuz khan <mahfuzpust@gmail.com>
2023-02-02 14:49:24 +0600

    Add_URL-services

---
+++ MagicVilla_Web/Services/BaseService.cs
@@ -0,0 +1,24 @@
+﻿using MagicVilla_Web.Models;
+using MagicVilla_Web.Services.IServices;
+using System.Text;
+
+namespace MagicVilla_Web.Services
+{
+    public class BaseService : IBaseService
+    {
+
+        public APIResponse responseModel { get; set; }
+        public IHttpClientFactory httpClient { get; set; }
+        public object JsonConvert { get; private set; }
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            this.responseModel = new();
+            this.httpClient = httpClient;
+        }
+
+        public Task<T> SendAsync<T>(APIRequest apiRequest)
+        {      
+        }
+    }
+}
